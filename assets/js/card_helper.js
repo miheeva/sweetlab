a078ba4cbca93aad22f87765c3df1ea0058a2f98
miheeva.siska <miheeva.aa@yandex.ru>
2017-07-19 00:34:19 +0300

    cart helper

--- assets/js/card_helper.js
+++ assets/js/card_helper.js
@@ -5,47 +5,69 @@
 
 
 $(document).ready(function() {
-   Quantity.init();
+    var cartItem = $('.woocommerce-cart-form__cart-item');
+    cartItem.each(function(key, value) {
+        new Quantity($(this));
+    });
 });
 
 
 
 
-
-    var Quantity = {
-        controls: {
-            deecriment: $('#qantityDeecriment'),
-            increement: $('#quantityIncreement')
-        },
-        countField: $('.quantity').find('input[type="number"]'),
-        count: 1,
-        price: parseFloat($('#card_window > div > div > div.row.content > form > div > div > div.col-xl-3.col-lg-3.col-md-3.col-sm-3.col-xs-3.product-name > span').text()),
-        totalPriceField: $('#card_window > div > div > div.row.content > form > div > div > div.col-xl-1.col-lg-1.col-md-1.col-sm-1.col-xs-1.product-subtotal > span'),
-        init: function() {
-            var self = this;
-          this.controls.deecriment.click(function(event) {
-             self.deecriment();
-          });
-          this.controls.increement.click(function(event) {
-              self.increement();
-          });
-        },
-        deecriment: function() {
-            if (this.count > 0) {
-                this.count--;
-                this.countField.val(this.count);
-                this.makeTotalPrice();
-            }
-        },
-        increement: function() {
-                this.count++;
-                this.countField.val(this.count);
-                this.makeTotalPrice();
-        },
-        makeTotalPrice: function() {
-            this.totalPriceField.text(this.count * this.price  + 'KZT');
-        }
+/**
+     *
+     * @param jQuery||element cartItem
+     * @constructor
+     */
+    var Quantity = function(cartItem) {
+        this.cartItem = cartItem;
+        this.orderTotal = $('.order-total').find('.woocommerce-Price-amount');
+        this.controls = {
+            deecriment: cartItem.find('#qantityDeecriment'),
+            increement: cartItem.find('#quantityIncreement')
+        };
+        this.countField = cartItem.find('.quantity').find('input[type="number"]');
+        this.count = this.countField.val();
+        this.price = parseFloat(cartItem.find('.product-name > span').text());
+        this.totalPriceField  = cartItem.find('div.col-xl-1.col-lg-1.col-md-1.col-sm-1.col-xs-1.product-subtotal > span');
+        console.log(this);
+        this.init();
     };
 
+    Quantity.prototype.init = function() {
+        var self = this;
+        this.controls.deecriment.click(function(event) {
+            self.deecriment();
+        });
+        this.controls.increement.click(function(event) {
+            self.increement();
+        });
+    };
+    Quantity.prototype.deecriment = function() {
+        if (this.count > 0) {
+            this.count--;
+            this.countField.val(this.count);
+            this.makeTotalPrice();
+        }
+    };
+    Quantity.prototype.increement = function() {
+        this.count++;
+        this.countField.val(this.count);
+        this.makeTotalPrice();
+    };
+
+    Quantity.prototype.makeTotalPrice = function () {
+        this.totalPriceField.text(this.count * this.price  + ' KZT');
+        this.makeTotalOrderPrice();
+    };
+    Quantity.prototype.makeTotalOrderPrice = function() {
+        var totalPrice = 0;
+        var itemsSumm = $('.woocommerce-cart-form__cart-item').find('div.col-xl-1.col-lg-1.col-md-1.col-sm-1.col-xs-1.product-subtotal > span');
+        console.log(this.orderTotal);
+        itemsSumm.each(function(key, value) {
+            totalPrice += parseFloat($(this).text());
+        });
+        this.orderTotal.text(totalPrice + ' KZT');
+    };
 
 })(jQuery);
